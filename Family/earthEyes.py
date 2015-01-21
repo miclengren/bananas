bbb13666e2718d0bd6fe9e45edefd04f42ffa296
miclengren <miclengren@gmail.com>
2015-01-21 07:38:55 -0600

    What 4.0

--- Family/earthEyes.py
+++ Family/earthEyes.py
@@ -11,19 +11,19 @@ import os.path
 directory = os.path.dirname(os.path.abspath(__file__))  
 backdrop = os.path.join(directory, 'backdrop.png')
 print backdrop
-# Open and show the student image in a new Figure window
+# Open and show image in a new Figure window
 backdrop_img = PIL.Image.open(backdrop)
 fig, axes = plt.subplots(1, 2)
 axes[0].imshow(backdrop_img, interpolation='none')
 
-# Display student in second axes and set window to the right eye
+# Display in second axes and set window 
 axes[1].imshow(backdrop_img, interpolation='none')
-axes[1].set_xticks(range(1050, 1410, 100))
-axes[1].set_xlim(1000, 1000) #coordinates measured in plt, and tried in iPython
-axes[1].set_ylim(1000,1000)
+axes[1].set_xticks(range(1150, 500, 10))
+axes[1].set_xlim(1150, 500) #coordinates measured in plt, and tried in iPython
+axes[1].set_ylim(1150,500)
 fig.show()
 
-# Open, resize, and display earth
+# Open, resize, and display
 family_member = os.path.join(directory, 'chyna.png')
 family_member_img = PIL.Image.open(family_member)
 fig2, axes2 = plt.subplots(1, 2)
@@ -31,13 +31,13 @@ axes2[0].imshow(family_member_img)
 
 fig2.show()
 
-# Paste earth into right eye and display
+# Paste into and display
 # Uses alpha from mask
-backdrop_img.paste(family_member_img, (1000, 1000), mask=family_member_img) 
+backdrop_img.paste(family_member_img, (1150, 500), mask=family_member_img)
 # Display
 fig3, axes3 = plt.subplots(1, 2)
 axes3[0].imshow(backdrop_img, interpolation='none')
 axes3[1].imshow(backdrop_img, interpolation='none')
-axes3[1].set_xlim(1000, 1000)
-axes3[1].set_ylim(1000, 1000)
+axes3[1].set_xlim(1150, 500)
+axes3[1].set_ylim(1150, 500)
 fig3.show()
