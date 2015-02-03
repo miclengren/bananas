c6f80fd8533d2ce7887b05aa8bc3d8098b4e571a
miclengren <miclengren@gmail.com>
2015-02-03 08:07:37 -0600

    hnmryj

--- Family/earthEyes.py
+++ Family/earthEyes.py
@@ -9,22 +9,22 @@ import os.path
 
 # Open the files in the same directory as the Python script
 directory = os.path.dirname(os.path.abspath(__file__))  
-backdrop = os.path.join(directory, 'backdrop.png')
+backdrop = os.path.join(directory, 'backdrop.jpg')
 print backdrop
 # Open and show image in a new Figure window
-backdrop_img = PIL.Image.open(backdrop)
+backdrop_img = PIL.Image.open('backdrop.jpg')
 fig, axes = plt.subplots(1, 2)
-axes[0].imshow(backdrop_img, interpolation='none')
+axes[0].imshow(backdrop_img, interpolation='chyna.jpg')
 
 # Display in second axes and set window 
-axes[1].imshow(backdrop_img, interpolation='none')
+axes[1].imshow(backdrop_img, interpolation='backdrop.jpg')
 axes[1].set_xticks(range(1150, 500, 10))
 axes[1].set_xlim(1150, 500) #coordinates measured in plt, and tried in iPython
 axes[1].set_ylim(1150,500)
 fig.show()
 
-# Open, resize, and display
-family_member = os.path.join(directory, 'chyna.png')
+# Open and display
+family_member = os.path.join(directory, 'chyna.jpg')
 family_member_img = PIL.Image.open(family_member)
 fig2, axes2 = plt.subplots(1, 2)
 axes2[0].imshow(family_member_img)
@@ -33,11 +33,11 @@ fig2.show()
 
 # Paste into and display
 # Uses alpha from mask
-backdrop_img.paste(family_member_img, (1150, 500), mask=family_member_img)
+backdrop_img.paste(family_member, (1150, 500), mask=family_member_img)
 # Display
 fig3, axes3 = plt.subplots(1, 2)
-axes3[0].imshow(backdrop_img, interpolation='none')
-axes3[1].imshow(backdrop_img, interpolation='none')
+axes3[0].imshow(backdrop, interpolation='none')
+axes3[1].imshow(backdrop, interpolation='none')
 axes3[1].set_xlim(1150, 500)
 axes3[1].set_ylim(1150, 500)
 fig3.show()
